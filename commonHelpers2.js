380998dc00fbfa4f4e2e6138c79ad472caba26b3
KittsWolves0 <114852364+KittsWolves0@users.noreply.github.com>
2024-05-30 16:15:29 +0000

    Deploying to gh-pages from @ KittsWolves0/goit-js-hw-10@b408f954545db496e0a677803d4493b6ce8ab0d7 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const o=new Promise((e,s)=>{setTimeout(()=>{e("Success! Value passed to resolve function")},2e3)});console.log(o);
+//# sourceMappingURL=commonHelpers2.js.map
