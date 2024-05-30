c2693e285d8aea3e684a397ea17ea40d255ef67a
Darina Ivashchenko <kittswolves@gmail.com>
2024-05-30 21:25:26 +0300

    task 2 done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,13 +1,46 @@
-const isSuccess = true;
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve("Success! Value passed to resolve function");
+
+const form = document.querySelector(".form");
+const deley = form.querySelector('[name="delay"]');
+const subBtn = form.querySelector('button');
+
+
+subBtn.addEventListener("click", notificatCreator);
+
+function notificatCreator(event) {
+  event.preventDefault();
+
+  const selectedOption = form.querySelector('input[name="state"]:checked');
+
+  const promises = () => {
+    if (selectedOption.value === "fulfilled") {
+      return Promise.resolve(deley.value);
     } else {
-      reject("Error! Error passed to reject function");
+      return Promise.reject(deley.value);
     }
-  }, 2000);
-});
+  };
 
-console.log(promise); // Об'єкт промісу
+  promises()
+    .then(deley => {
+      setTimeout(() => { iziToast.show({
+            message: `✅Fulfilled promise in ${deley}ms`,
+            position: 'topRight',
+            messageColor: 'white',
+            backgroundColor: 'green'
+        }); }, deley);
+    })
+    .catch(deley => {
+      setTimeout(() => { iziToast.show({
+            message: `❌ Rejected promise in ${deley}ms`,
+            position: 'topRight',
+            messageColor: 'white',
+            backgroundColor: 'red'
+        }); }, deley);
+    })
+
+  form.reset();
+}
